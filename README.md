a66fbc154b25a7bb1862662dda467e6dba2e9e0b
SantajitPatra <business.santajitpatra@gmail.com>
2023-05-09 23:55:52 +0530

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Good Books Web App
+
+This is a web application that allows users to browse and discover good books to read. The app is built using React JS and leverages the Books API to fetch book data.
+
+## Getting Started
+
+To get started with the app, you will need to have Node.js and NPM installed on your machine. Once you have those installed, you can follow these steps to run the app:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory in your terminal.
+3. Run `npm install` to install the required dependencies.
+4. Run `npm start` to start the development server.
+5. Open your browser and navigate to `http://localhost:3000`.
+
+## Features
+
+The Good Books web app has the following features:
+
+- **Book search:** Users can search for books by title, author, or ISBN. The search results are displayed in a grid view with book covers, titles, authors, and descriptions.
+
+- **Book details:** Users can click on a book in the search results to view more details about the book, including its cover, title, author, description, and other metadata.
+
+- **Bookshelves:** Users can add books to their bookshelves by clicking the "Add to Bookshelf" button on a book's details page. Bookshelves are stored in local storage and can be accessed later.
+
+## Contributing
+
+If you would like to contribute to the Good Books web app, you can submit a pull request with your changes. Before submitting a pull request, please ensure that your code follows the existing code style and passes all tests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const bookDB = {
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" }
+  ],
+
+  fiction: [
+    {
+      name: "Shiva Trilogy",
+      rating: "5/5"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "4.5/5"
+    }
+  ],
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedGenre, setGenre] = useState("business");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> 📖 Good Books 📕📘📙📗 </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+          key={genre}
+            onClick={() => genreClickHandler(genre)}
+            id="book_genre"
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {bookDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+             className="book_name"
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
